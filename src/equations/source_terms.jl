9cb2876971b0458dacc3e71dd046e25d5619fe18
Marie Boussard <74459375+marieboussard@users.noreply.github.com>
2024-07-09 15:44:47 +0200

    source term management

--- src/equations/source_terms.jl
+++ src/equations/source_terms.jl
@@ -1,8 +1,3 @@
-#include("abstract_equations.jl")
-#include("../methods/rusanov.jl")
-#include("../domain.jl")
-#include("../methods/hydrostatic.jl")
-
 abstract type Source end
 struct NullSource <: Source end
 
@@ -15,14 +10,14 @@ bump_zb(height=3) = Bump_zb(height)
 
 zb(::NullSource, x) = zero(x)
 
-zb(bz::Bump_zb, x) = (-0.5 .* x .* (-1 .+ x)) .* bz.height
-Dzb(bz::Bump_zb, x) = (-x .+ 0.5) .* bz.height
+# zb(bz::Bump_zb, x) = (-0.5 .* x .* (-1 .+ x)) .* bz.height
+# Dzb(bz::Bump_zb, x) = (-x .+ 0.5) .* bz.height
 
 # zb(bz::Bump_zb, x) = x .* bz.height
 # Dzb(bz::Bump_zb, x) = (ones(size(x))) .* bz.height
 
-# zb(bz::Bump_zb, x) = -0.1 * (cos.(2*pi * x) .- 1)*bz.height
-# Dzb(bz::Bump_zb, x) = 0.1 * pi * sin.(2*pi * x)*bz.height
+zb(bz::Bump_zb, x) = -0.1 * (cos.(2*pi * x) .- 1)*bz.height
+Dzb(bz::Bump_zb, x) = 0.1 * pi * sin.(2*pi * x)*bz.height
 
 # zb(::Bump_zb, x) = zero(x)
 # Dzb(::Bump_zb, x) = zero(x)
@@ -30,11 +25,17 @@ Dzb(bz::Bump_zb, x) = (-x .+ 0.5) .* bz.height
 zb_tilde(::Bump_zb, ut, c) = c - ut[1]
 
 #sourceTerm(::FVMethod, zbSource::ZbSource, domain::Domain, v) = [[0.0, -v[i][1] * g * Dzb(zbSource, domain.x)[i]] for i in eachindex(v)]
+
 function sourceTerm(::FVMethod, zbSource::ZbSource, domain::Domain, v)
     S = zero(v)
     for i in 1:length(v[:,1])
         S[i,1] = 0.0
-        S[i,2] = -v[i,1]*g*Dzb(zbSource, domain.x[i])
+        S[i,2] = -v[i,1]*g*domain.DSourceVec[i]
     end
     S
+end
+
+function addSource!(zbSource::ZbSource, domain::Domain)
+    domain.sourceVec = zb(zbSource, domain.x)
+    domain.DSourceVec = Dzb(zbSource, domain.x)
 end
