6df63309145e3b98c6d8b33cd01cc15c769cf027
Marie Boussard <74459375+marieboussard@users.noreply.github.com>
2024-10-01 16:15:00 +0200

    Testing diffusion compensation

--- src/diffusion_compensation.jl
+++ src/diffusion_compensation.jl
@@ -1,3 +1,96 @@
+function compute_G_mid_f_delta(f_delta::AbstractArray{T}, sol::OptForEntropySol) where T
+
+    equation, method, domain, modifiedDataType, boundsType = sol.equation, sol.method, sol.domain, sol.modifiedDataType, sol.boundsType
+    Nx, dx, dt = domain.Nx, domain.dx, sol.dt_vec[end]
+    lambd = dt/dx
+    sL, sR = get_sL(method), get_sR(method)
+    u, up = sol.u_approx[end-1], sol.u_approx[end]
+    # source
+    z = isnothing(domain.sourceVec) ? zeros(T, (Nx, 1)) : reshape(domain.sourceVec, (domain.Nx,1))
+
+    M_delta, m_delta = zeros(T, Nx+1), zeros(T, Nx+1)
+    for j in 1:Nx
+        # Calcul of Bg delta
+        ut = compute_u_tilde(modifiedDataType, u, j, sL, sR)
+        zt = isnothing(domain.sourceVec) ? zeros(T, size(ut)) : compute_u_tilde(modifiedDataType, z, j, sL, sR)
+            
+                ###### This part needs to be completed for equation with source term
+        Dzt = isnothing(domain.DSourceVec) ? zeros(T, size(ut)) : domain.DSourceVec
+        uh = copy(ut)
+        #sourceVec = isnothing(sourceTerm(method, domain, ut; z=zt)) ? zero(Nx) : sourceTerm(method, domain, ut; z=zt)
+        sourceVec = sourceTerm(equation, method, domain, ut; z=zt, Dz=Dzt)
+        for k in j-sL-sR+1:j+sR+sL
+            uh[mod1(k, Nx), :] = ut[mod1(k, Nx), :] .- lambd .* (
+                giveNumFlux(method, equation, ut[mod1(k, Nx), :], ut[mod1(k + 1, Nx), :]; zL=zt[mod1(k, Nx)], zR=zt[mod1(k + 1, Nx)])
+                .-
+                giveNumFlux(method, equation, ut[mod1(k - 1, Nx), :], ut[mod1(k, Nx), :]; zL=zt[mod1(k - 1, Nx)], zR=zt[mod1(k, Nx)])) .+ dt * sourceVec[mod1(k, Nx),:]
+                .-
+                lambd*(f_delta[mod1(k, Nx)] - f_delta[mod1(k-1, Nx)])
+        end
+
+        m, M = initBounds(modifiedDataType, equation, u, j, sL, sR, z)
+        m, M = updateBounds!(modifiedDataType, boundsType, equation, m, M, ut, uh, j, sL, sR, Nx, dx, dt, zt)
+
+        m_delta[j+1], M_delta[j+1] = m[1], M[1]
+    end
+
+    0.5*(m_delta.+M_delta)
+
+end
+
+function compute_G_mid(A::AbstractArray{T}, sol::OptForEntropySol) where T
+    compute_G_mid_f_delta(f_delta_from_A(A, sol), sol)
+end
+
+function modified_scheme_step_f_delta(f_delta::AbstractArray{T}, sol::OptForEntropySol) where T
+    equation, method, domain, modifiedDataType, boundsType = sol.equation, sol.method, sol.domain, sol.modifiedDataType, sol.boundsType
+    Nx, dx, dt = domain.Nx, domain.dx, sol.dt_vec[end]
+    lambd = dt/dx
+    sL, sR = get_sL(method), get_sR(method)
+    u = sol.u_approx[end-1]
+    # source
+    z = isnothing(domain.sourceVec) ? zeros(T, (Nx, 1)) : reshape(domain.sourceVec, (domain.Nx,1))
+    
+    p = get_unknowns_number(equation)
+    numericalFluxMat = zeros(T, (Nx+1, p))
+    up = zeros(T, (Nx, p))
+
+    for i ∈ 2:Nx
+        numericalFluxMat[i,:] = giveNumFlux(method, equation, u[i-1,:], u[i,:]; zL=z[i-1], zR=z[i])
+    end
+    numericalFluxMat[1,:] = giveNumFlux(method, equation, u[end,:], u[1,:]; zL=z[end], zR=z[1])
+    numericalFluxMat[end,:] = numericalFluxMat[1,:]
+
+    for j in 1:Nx
+        up[j] = u[j] - lambd*(numericalFluxMat[j] - numericalFluxMat[mod1(j-1,Nx)]) - lambd*(f_delta[j+1] - f_delta[j])
+    end
+
+    up
+
+end
+
+function modified_scheme_step(A::AbstractArray{T}, sol::OptForEntropySol) where T
+    modified_scheme_step_f_delta(f_delta_from_A(A, sol), sol)
+end
+
+function diffusion_from_A(A::AbstractArray{T}, sol::OptForEntropySol) where T
+
+    # Extracting data from solution of entropy optimizing process
+    domain, equation, u = sol.domain, sol.equation, sol.u_approx[end-1]
+    Nx, dx, dt = domain.Nx, domain.dx, sol.dt_vec[end]
+    lambd = dt/dx
+    z = isnothing(domain.sourceVec) ? zeros(T, (Nx,1)) : domain.sourceVec
+
+    # Constructing approximated solution and numerical entropy flux for modified scheme associated with A
+    f_delta = f_delta_from_A(A, sol)
+    G_mid = compute_G_mid_f_delta(f_delta, sol)
+    up = modified_scheme_step_f_delta(f_delta, sol)
+    
+    # Calculating numerical diffusion
+    [get_eta(equation, up[i,:]; z=z[i])[1] - get_eta(equation, u[i,:]; z=z[i])[1] for i in 1:length(u[:,1])] + dt / dx * (G_mid[2:end] - G_mid[1:end-1])
+end
+
+
 function constraint_f_delta(f_delta::AbstractArray{T}, sol::OptForEntropySol) where T
     
     equation, method, domain, modifiedDataType, boundsType = sol.equation, sol.method, sol.domain, sol.modifiedDataType, sol.boundsType
@@ -50,7 +143,7 @@ function constraint_f_delta(f_delta::AbstractArray{T}, sol::OptForEntropySol) wh
 
         bg_delta[j] = lambd*(((M_delta[j+1]+m_delta[j+1])/2 - sol.Gopt[j+1]) - ((M_delta[j]+m_delta[j])/2 - sol.Gopt[j]))
         
-        cons += max(0, min(0,-sol.Dopt[j])-bu_delta[j]-bg_delta[j])^2
+        cons += min(0, min(0,-sol.Dopt[j])-bu_delta[j]-bg_delta[j])^2
         
     end
     cons
@@ -61,7 +154,13 @@ end
 function f_delta_from_A(A::AbstractArray{T}, sol::OptForEntropySol) where T
 
     # A should be a vector
+
+    # First case : we use only initial data
     u = sol.u_approx[end-1]
+
+    # # Second case : we use data estimated by non entropic scheme
+    # u = sol.u_approx[end]
+
     Nx, p = size(u)
     f_delta = zeros(T, (Nx+1, p))
     for i in 1:Nx+1
@@ -76,7 +175,7 @@ function constraint_A(A, sol::OptForEntropySol)
 end
 
 function find_optimal_A(sol::OptForEntropySol)
-    @show A_init, _p = zero(sol.Gopt).-3, 0.0
+    @show A_init, _p = zero(sol.Gopt), 0.0
     cons(res, A, p) = (res .= [constraint_A(A, sol)])
     @show constraint_A(A_init, sol)
 
@@ -85,7 +184,7 @@ function find_optimal_A(sol::OptForEntropySol)
     #optprob = OptimizationFunction((x,p) -> abs.(x), Optimization.AutoZygote(); cons = cons)
     #optprob = OptimizationFunction((x,p) -> abs.(x); cons = cons)
     # optprob = OptimizationFunction((x,p) -> abs.(x), Optimization.AutoModelingToolkit(); cons = cons)
-    prob = OptimizationProblem(optprob, A_init, _p, lcons = [-Inf], ucons = [0.0])
+    prob = OptimizationProblem(optprob, A_init, _p, lcons = [-10.0], ucons = [0.0]) # -Inf
     #solA = solve(prob, IPNewton())
     #solA = solve(prob, BFGS())
     solA = solve(prob, Ipopt.Optimizer())
--- tests/diff_compensate_test.jl
+++ tests/diff_compensate_test.jl
@@ -2,18 +2,52 @@ include("../src/include_file.jl")
 
 # 1 # Solving Burgers equation
 
-xmin, xmax, Nx, t0, Tf = -2, 2, 10, 0, 0.4
+xmin, xmax, Nx, t0, Tf = -2, 2, 20, 0, 0.4
 CFL_factor = 0.5
 domain = createInterval(xmin, xmax, Nx, t0, Tf)
-u0 = (res=zeros(domain.Nx, 1); for i in 1:Nx res[i,:]=[u0_burgers_article(domain.x[i])] end; res)
+#global u0 = (res=zeros(domain.Nx, 1); for i in 1:Nx res[i,:]=[u0_burgers_article(domain.x[i])] end; res)
 equation = burgers()
-method = Roe(CFL_factor)
+#method = Roe(CFL_factor)
+method = Rusanov(CFL_factor)
+# dx = domain.dx
+# dt = method.CFL_factor * dx / CFL_cond(equation, u0)
+# domain.Tf = dt # Tf is modified so that only one timestep is performed
+u0 = (res=zeros(domain.Nx, 1); for i in 1:Nx res[i,:]=[u0_burgers_article(domain.x[i])] end; res)
+
+T_reached = 0.0
+
 
 # solBurgers = fv_solve(domain, u0, equation, method)
 # plot_fv_sol(solBurgers, uexact_burgers_article)
+u0_temp = u0
+for k in 1:5
+    global u0_temp
+    global T_reached
+    global solEnt
+    global Aopt
 
-solEnt = optimize_for_entropy(u0, domain, equation, method)
-plot_solution(solEnt)
+    dx = domain.dx
+    dt = method.CFL_factor * dx / CFL_cond(equation, u0_temp)
+    domain.Tf = dt # Tf is modified so that only one timestep is performed
+    T_reached += dt
+    solEnt = optimize_for_entropy(u0_temp, domain, equation, method)
+    solA = find_optimal_A(solEnt);
+    Aopt = solA.u
+    u0_temp = modified_scheme_step(Aopt, solEnt)
+
+    Gmid = compute_G_mid(Aopt, solEnt)
+    plot(domain.interfaces, Gmid, label="G mid")
+    plot!(domain.interfaces, solEnt.Gopt, label="Gopt")
+    display(title!("t="*string(round(T_reached, sigdigits=3))))
+
+    Dmid = diffusion_from_A(Aopt, solEnt)
+    plot(domain.x, Dmid, label="Dmid")
+    plot!(domain.x, solEnt.Dopt, label="Dopt")
+    display(title!("t="*string(round(T_reached, sigdigits=3))))
+end
+
+@show T_reached
+#plot_solution(solEnt)
 
 # g_delta = B_delta(solEnt)
 # display(plot(domain.interfaces, g_delta))
@@ -22,4 +56,20 @@ plot_solution(solEnt)
 # plot(domain.x, D_test)
 # plot!(domain.x, solEnt.Dopt./10)
 
-solA = find_optimal_A(solEnt);
+#solA = find_optimal_A(solEnt);
+#Aopt = solA.u
+#up_modified = modified_scheme_step(Aopt, u0_temp)
+up_modified = u0_temp
+# Final resolution
+domain.Tf = T_reached
+solEnt = optimize_for_entropy(u0, domain, equation, method)
+solRus = fv_solve(domain, u0, equation, Rusanov(CFL_factor))
+
+# Gmid = compute_G_mid(Aopt, solEnt)
+# plot(domain.interfaces, Gmid, label="G mid")
+# display(plot!(domain.interfaces, solEnt.Gopt, label="Gopt"))
+
+
+plot(domain.x, up_modified, label="modified "*get_name(method))
+plot!(domain.x, solEnt.u_approx[end], label=get_name(method))
+plot!(domain.x, solRus.u_approx[end], label="Rusanov")
