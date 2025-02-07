69ef735830aa069e3a3151d5890ab874412f5e1e
Marie Boussard <74459375+marieboussard@users.noreply.github.com>
2025-02-07 11:15:07 +0100

    Performance estimation

--- tests/performance.jl
+++ tests/performance.jl
@@ -6,7 +6,7 @@ using ProfileView
 
 # 1.1 # Solving Burgers equation with Rusanov
 
-xmin, xmax, Nx, t0, Tf = -2, 2, 100, 0.0, 0.4
+xmin, xmax, Nx, t0, Tf = -2, 2, 10000, 0.0, 0.4
 CFL_factor = 0.5
 domain = Interval(Nx, xmin, xmax, t0, Tf)
 equation = burgers()
@@ -18,4 +18,6 @@ u_init = initialData(domain, testcase)
 uexact = exactData(domain, testcase)
 
 problem = FVProblem(domain, equation, testcase, scheme, saveLog, u_init)
-VSCodeServer.@profview fv_sol = solve(problem);
+#VSCodeServer.@profview 
+
+fv_sol = solve(problem);
