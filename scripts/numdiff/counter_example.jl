8c359fced6916f01a6c1d40014706638db512787
Marie Boussard <74459375+marieboussard@users.noreply.github.com>
2025-03-18 14:06:53 +0100

    Counter example script

---
+++ scripts/numdiff/counter_example.jl
@@ -0,0 +1,60 @@
+using BenchmarkTools
+include("../../src/numdiff/include_file.jl")
+
+# PARAMETRIZED INITIAL DATA
+
+const a1 = 1.0
+const b1 = 2.0
+const a2 = 2.0
+const b2 = 4.5
+
+xmin, xmax = -b1/a1, b2/a2
+M = 100
+Nx = Int((1 + floor(M/(a1*b2+a2*b1)))*(a2*b1+a1*b2))
+t0, tf = 0.0, 0.2
+CFL_factor = 0.5
+mesh = OneDMesh(Nx, xmin, xmax)
+params = Parameters(mesh, t0, tf, CFL_factor)
+
+# INITIAL CONDITION
+u0(x::Real) = x<=0 ? -a1*x-b1 : -a2*x+b2
+u0(x::AbstractArray) = u0.(x)
+
+# EXACT SOLUTION
+function uexact_fun(x::Real, t::Real)
+    if t >= min(1/a1, 1/a2)
+        @warn "Warning: This solution is not valid for t ≥ min(1/a1, 1/a2)"
+    end
+    if x <= -b1*t
+        return (a1*x+b1) / (a1*t-1)
+    elseif x >= b2*t
+        return (a2*x-b2) / (a2*t-1)
+    else
+        return x/t
+    end
+end
+uexact = [uexact_fun(xi, sol.t) for xi in mesh.x]
+
+# FINITE VOLUMES RESOLUTION
+equation = Equation(OneD(), 1, Scalar(), Burgers(), u0)
+sol = solve(equation, params, Euler(), Roe(); log_config=LogConfig(true, false, true, false));
+
+using Plots
+plot(mesh.x, sol.uinit, label="uinit")
+plot!(mesh.x, uexact, label="exact")
+display(plot!(mesh.x, sol.u, label="t = "*string(sol.t)))
+
+# QUANTIFICATION OF NUMERICAL DIFFUSION
+estimate_priori = quantify_diffusion(sol, PrioriMultidim(); name="priori multidim");
+estimate_posteriori = quantify_diffusion(sol, Posteriori(AsymmetricMD()); name="asymmetric posteriori");
+
+# CHECKING COUNTER EXAMPLE CONDITIONS
+
+println("min(M-m):")
+println(minimum(estimate_posteriori.M .- estimate_posteriori.m))
+println("min(L-l)")
+println(minimum(estimate_priori.L .- estimate_priori.l))
+println("sum(L)")
+println(sum(estimate_priori.L))
+println("sum(l)")
+println(sum(estimate_priori.l))
--- scripts/numdiff/numdiff_multi_burgers.jl
+++ scripts/numdiff/numdiff_multi_burgers.jl
@@ -2,7 +2,7 @@ using BenchmarkTools
 include("../../src/numdiff/include_file.jl")
 
 # Domain definition
-Nx = 100
+Nx = 30
 xmin, xmax = -2, 2
 t0, tf = 0.0, 0.4
 CFL_factor = 0.5
@@ -14,11 +14,31 @@ equation = BurgersArticle
 sol = solve(equation, params, Euler(), Rusanov(); log_config=LogConfig(true, false, true, false));
 
 estimate_priori = quantify_diffusion(sol, PrioriMultidim(); name="priori multidim");
+estimate_posteriori = quantify_diffusion(sol, Posteriori(AsymmetricMD()); name="asymmetric posteriori");
+
+# EXACT NUMERICAL ENTROPY FLUX
+estimator = Estimator(sol, Posteriori(AsymmetricMD()));
+eta!(estimator.entfun, estimator.uinit, estimator.etacont_init)
+eta!(estimator.entfun, estimator.u, estimator.etacont)
+compute_G_bounds!(estimator)
+etavec = zero(mesh.x)
+Gvec = zero(mesh.x)
+eta!(BurgersEnt(), estimate_posteriori.uinit, etavec)
+G!(BurgersEnt(), estimate_posteriori.uinit, Gvec)
+Gexact = zero(mesh.x)
+for i in 1:Nx
+    Gexact[i] = 0.5*(Gvec[i] + Gvec[mod1(i+1,Nx)]) - max(abs(estimate_posteriori.uinit[i]), abs(estimate_posteriori.uinit[mod1(i+1,Nx)]))*0.5*(etavec[mod1(i+1,Nx)] - etavec[i])
+end
+# Gback = Gexact[mod1.(Nx:1, Nx)]
+Gback = [Gexact[Nx]; Gexact[1:Nx-1]]
+DGexact = estimate_posteriori.dt / mesh.dx *(Gexact .- Gback)
 
 using Plots
-plot(mesh.x, estimate.uinit, label="uinit")
+plot(mesh.x, estimate_priori.uinit, label="uinit")
 display(plot!(mesh.x, sol.u, label="t = "*string(sol.t)))
-plot(mesh.x, estimate.l, label="l")
-display(plot!(mesh.x, estimate.L, label="L"))
-plot(mesh.x, estimate.Dlow, label="Dlow")
-plot(mesh.x, estimate.D, label="Numerical Diffusion")
+plot(mesh.x, estimate_priori.l, label="l")
+plot!(mesh.x, DGexact, label="DGexact")
+display(plot!(mesh.x, estimate_priori.L, label="L"))
+plot(mesh.x, estimate_priori.Dlow, label="Dlow")
+plot!(mesh.x, estimate_posteriori.D, label=estimate_posteriori.name)
+plot!(mesh.x, estimate_priori.D, label=estimate_priori.name)
